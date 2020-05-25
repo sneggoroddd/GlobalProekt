227f5d394e818c282e906ffe276da9d38a486b4d
Your Name <sneggorod@mail.ru>
2020-05-25 22:08:10 +0300

    добавил Stars.py

--- Loops/multiplicationTable.py
+++ Loops/multiplicationTable.py
@@ -1,1 +1,7 @@
 '''Выведите на экран таблицу умножения для чисел от 1 до 10'''
+def main():
+     for i in range(1, 10):
+        for j in range(1, 10):
+            print((i*j), end=" ")
+        print("")
+main()
