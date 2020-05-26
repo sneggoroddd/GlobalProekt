805bc11dbc7c09e055b7f84ef53e5db30787c002
Your Name <sneggorod@mail.ru>
2020-05-26 12:23:39 +0300

    добавил Stars.py

--- Loops/goods.py
+++ Loops/goods.py
@@ -2,6 +2,7 @@ def main():
     N = int(input("Кол-во товаров: "))
     total = 0
     maxcost = 0
+    dopgood = 0
     for i in range(N):
         cost = int(input("Введите стоимость товара: "))
         total = total+ cost
@@ -9,8 +10,15 @@ def main():
             maxcost= cost
     print("Самый дорогой товар ", maxcost)
     print(" общая стоимость ", total)
+    if total >= 1000 and total < 2000:
+        print("Цена со скидкой", total*0.95, "Скидка 5%")
+    elif total >= 2000:
+        print("Цена со скидкой", total*0.9, 'Скидка 10%')
+    else:
+        dopgood = 1000 -total
+        print('купите еще на', dopgood, "И получите скидку 5%")
+
 main()
 
-    ''' Функция делает скидку на товар. 1
-    Если на 1000 то 5%, если больше 2000 то 10% '''
+
     
