e7aeef0adc5cbc3a3dd245e25fb345621ecf16bd
Your Name <sneggorod@mail.ru>
2020-05-26 12:43:33 +0300

    добавил Stars.py

---
+++ Loops/simpleNumber.py
@@ -0,0 +1,9 @@
+'''Определите, является ли данное число простым.'''
+def main()
+    number = 0
+    i = 1
+    number = int(input( "Введите число "))
+    while number/count = 1:
+        for i in range (number):
+            count = count + 1
+
