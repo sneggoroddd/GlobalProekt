f7723f5a46ef73efa482caa7ba15ecab0e1a463e
Your Name <sneggorod@mail.ru>
2020-06-01 10:19:28 +0300

    добавил игру- угадай числодобавил в нее возможность выхода и проверку баланса.

--- Loops/RPC.py
+++ Loops/RPC.py
@@ -5,9 +5,21 @@
 
 def main():
     end = 0
+    sto = 0
+    sci = 0
+    pap = 0
+    loss = 0
+    win = 0
+    math = 0
     import random
     while end == 0:
         count = int(input(" Введите Камень- 1 или  Ножницы-2 или Бумага-3 :"))
+        if count == 1:
+            sto += 1
+        elif count == 2:
+            sci += 1
+        else:
+            pap += 1
         computer = random.randint(1, 3)
 
         if computer == 1:
@@ -23,8 +35,13 @@ def main():
             print("Ничья")
         elif count == 1 and value == 2 or count == 2 and value == 3 or count == 3 and value == 1:
             print("Вы победили")
+            win += 1
         else:
             print("Вы проиграли, компьютер загадал ", computer)
+            loss += 1
+        math += 1
+        print(f" Всего игр {math}, побед {win}, поражений {loss}")
+        print(f"Вы выбрали камень {sto} раз, ножницы {sci} раз, бумага {pap} раз")
         end = int(input(' Продолжить да 0 нет 1'))
 main()
 
